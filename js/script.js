0a11556c6042a57cabfa8c3d2334d688464ef67c
Mdp14 <asrirekatama2017@gmail.com>
2024-08-07 11:57:49 +0700

    all files

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementById('whatsapp-button').addEventListener('click', function() {
+    document.getElementById('whatsapp-popup').style.display = 'block';
+});
+
+document.getElementById('close-popup').addEventListener('click', function() {
+    document.getElementById('whatsapp-popup').style.display = 'none';
+});
